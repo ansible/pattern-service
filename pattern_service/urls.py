b5285eaad963cf22495cbe259a7a4225f3f7befd
Helen Bailey <hebailey@redhat.com>
2025-06-16 12:57:29 -0400

    Updates for aap-dev

--- pattern_service/urls.py
+++ pattern_service/urls.py
@@ -18,6 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import ping, test
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("ping/", ping),
+    path("api/pattern-service/v1/test/", test),
 ]
