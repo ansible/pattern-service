28ef8a5e8cf1bff10f1b9c13216ec54cc78220cf
aubin bikouo <abikouo@redhat.com>
2025-07-11 16:13:01 +0200

    move

--- src/pattern_service/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Automation, ControllerLabel, Pattern, PatternInstance, Task
-
-admin.site.register(Pattern)
-admin.site.register(ControllerLabel)
-admin.site.register(PatternInstance)
-admin.site.register(Automation)
-admin.site.register(Task)
